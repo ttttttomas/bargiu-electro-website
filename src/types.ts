a31f122a34cfc6e58b9debfca6288275fedec54f
Tomas <totobarajas124@gmail.com>
2025-08-29 14:07:36 -0300

    error merge types

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,8 @@
 export interface Product {
-<<<<<<< HEAD
-  id: string;
-  title: string;
-  price: number;
-  category: string;
   sub_categorys: string[];
   sub_category_main: string;
-  main_image: string;
   images: string[];
   details_list: string[];
-  stock: boolean;
-  height: number;
-  width: number;
-  depth: number;
-=======
   id: number;
   main_image: string;
   title: string;
@@ -29,5 +18,4 @@ export interface Product {
   image_2: string;
   image_3: string;
   image_4: string;
->>>>>>> 1243bbd38c9f3328dc48dc1e32f74d030535e023
 }
